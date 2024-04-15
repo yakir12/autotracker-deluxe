6d740d81dacc3df56442956d66f5859ca450a3a5
Robert Mitchell <r.mitchell@ed.ac.uk>
2024-04-15 13:42:57 +0100

    Started building a new calibration script
    Started a new calibration script based on randomly selecting
    frames from the calibration video in which the chessboard
    could be found; extrinsic calibration frames have to be specified
    manually.
    
    The script then builds a cache of the images in which the
    checkerboard was found and the coordinates of the corners.
    
    This could be extended later on to allow a user to specify their
    own image files, or using the video pause/select method we
    currently employ.

--- homo.py
+++ homo.py
@@ -1,6 +1,16 @@
+"""
+Test script for honing calibration procedure.
+
+Randomly extract n frames from a test calibration video which have the chessboard
+present. An extrinsic frame is hard coded for the test video
+
+"""
+
+
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
+import os
 
 def define_object_chessboard(n_rows, n_columns, square_size,):
     """
@@ -35,54 +45,127 @@ def undistort_image(frame, mtx=None, dist=None):
                                distCoeffs=dist)
     return frame_dist
 
+def cache_calibration_video_frames(video_path, 
+                                   object_chessboard,
+                                   chessboard_size,
+                                   N=15, 
+                                   extrinsic_frames=0,
+                                   frame_cache='calibration_image_cache'):
+    """
+    Select N frames from a calibration video where the chessboard is successfully
+    found.
+
+    :param video_path: The filepath to the calibration video
+    :param object_chessboard: An OpenCV image of the object-space chessboard.
+    :param chessboard_size: The dimensions of the chessboard (inner corners).
+    :param N: the number of frames you want to find.
+    :param extrinsic_frame: The index of a frame which can be used for extrinsic
+                            calibration (the chessboard is on the ground). This
+                            is guessed to be the first frame but SHOULD BE 
+                            PROVIDED BY THE CALLER!
+    :param frame_cache: The caching directory to use for calibration frames.
+    :returns: The filepath to the frames.
+    """
+
+    # Ensures randomness is repeatable.
+    random_state = np.random.RandomState()
+
+
+    cap = cv2.VideoCapture(video_path)
+    frame_count = cap.get(cv2.CAP_PROP_FRAME_COUNT)
+    sample_indices = random_state.choice(range(int(frame_count)),
+                                         size=int(N),
+                                         replace=False)
+
+
+    # Cache appropriate images for intrinsic calibration
+    failed_indices = []
+    idx = 0
+    while idx < len(sample_indices):
+        frame_idx = sample_indices[idx]
+
+        print("Checking frame {}".format(frame_idx))
+
+        # Set capture position and get frame
+        cap.set(cv2.CAP_PROP_POS_FRAMES, frame_idx)
+        _, frame = cap.read()
+
+        chessboard_found, corners = cv2.findChessboardCorners(frame,
+                                                              chessboard_size)
+        if not chessboard_found:
+            print("Failed to find chessboard in frame, trying a new frame...")
+            failed_indices.append(frame_idx) # Keep track of failed frames
+            new_choice = frame_idx
+            
+            # Look for a new (random) frame index which hasn't been tried already
+            # and isn't in the current sample_indices list.
+            while ((new_choice in failed_indices) or (new_choice in sample_indices)):
+                new_choice = random_state.choice(range(int(frame_count)), size=1)
+
+            # Replace the current frame with the new one to try again.
+            sample_indices[idx] = new_choice
+            
+            continue # continue without index increment to try again.
+
+        # If the chessboard was found, then save a copy of the image with
+        # the corners drawn and a clean copy.
+        filepath = os.path.join(frame_cache, "intrinsic", "{:03d}.png".format(idx))
+        corner_file = os.path.join(frame_cache, "intrinsic", "corners", "{:03d}.dat".format(idx))
+        print("Chessboard found, caching image at {}".format(filepath))
+        cv2.imwrite(filepath, frame)
+        corners.dump(corner_file)
+        idx += 1
+
+    # Cache extrinsic image, these are pre-selected so if the chessboard can't
+    # be found, inform the user and move on. Only issue is if there was no
+    # successful extrinsic frame.
+    extrinsic_frames = np.atleast_1d(extrinsic_frames) # Make sure this is in array format
+    ext_success = False # Overall extrinsic calibration success
+    count = 0
+    for frame_idx in extrinsic_frames:
+        cap.set(cv2.CAP_PROP_POS_FRAMES, frame_idx)
+        print("Extrinsic, checking frame {}".format(frame_idx))
+        _, frame = cap.read()
+        
+        chessboard_found, corners = cv2.findChessboardCorners(frame,
+                                                              chessboard_size)
+        ext_success |= chessboard_found
+
+        if not chessboard_found:
+            print("Chessboard not found in frame {} for extrinsic calibration.")
+            continue
+
+        filepath = os.path.join(frame_cache, "extrinsic", "{:03d}.png".format(count))
+        corner_file = os.path.join(frame_cache, "extrinsic", "corners", "{:03d}.dat".format(count))
+        print("Chessboard found, caching image at {}".format(filepath))
+        cv2.imwrite(filepath, frame)
+        corners.dump(corner_file)
+
+        count += 1
+        
+    if not ext_success:
+        print("Warning: Chessboard not found in any frames chosen for extrinsic calibration!")
+
 if __name__ == "__main__":
     # Use calibration info to work out homography
     # Detect checkerboard in image then report distance between corners.
 
-    # Lifted from calibration dump
+    # Selected test video
+    video = "/home/robert/postdoc/source/output.mov"
+    
+    # Chessboard parameters
     n_rows = 6
     n_cols = 9
-    square_size = 39 # Sq. size in mm
+    square_size = 39
 
+    # Define an object chessboard image and convert to OpenCV-compatible type.
     object_chessboard, chessboard_size =\
           define_object_chessboard(n_rows, n_cols, square_size)
-    
-    # Turn it into a grayscale opencv image
     object_chessboard = object_chessboard.astype(np.uint8) * 255
-        
-    window = 'Test'
-    board = 'Board'
-    image = cv2.imread('test_image.png')
 
-    image = undistort_image(image)
-
-    
-
-
-    cv2.namedWindow(window, cv2.WINDOW_NORMAL)
-    cv2.namedWindow(board, cv2.WINDOW_NORMAL)
-
-    success, corners = cv2.findChessboardCorners(image, chessboard_size)
-    cv2.drawChessboardCorners(image,
-                              patternSize=chessboard_size,
-                              corners=corners, 
-                              patternWasFound=success)
-    
-    object_chessboard = cv2.cvtColor(object_chessboard, cv2.COLOR_GRAY2BGR)
-    success, corners = cv2.findChessboardCorners(object_chessboard, 
-                                                 chessboard_size)
-    cv2.drawChessboardCorners(object_chessboard,
-                              patternSize=chessboard_size,
-                              corners=corners, 
-                              patternWasFound=success)
-
-    cv2.imshow(window, image)
-    cv2.imshow(board, object_chessboard)
-    
-    while cv2.getWindowProperty(window, cv2.WND_PROP_VISIBLE):
-        if cv2.waitKey(1) == ord('q'):
-            break
-
-
-
-
+    refresh_cache=True
+    if refresh_cache:
+        cache_calibration_video_frames(video, 
+                                       object_chessboard, 
+                                       chessboard_size, 
+                                       N=30)
